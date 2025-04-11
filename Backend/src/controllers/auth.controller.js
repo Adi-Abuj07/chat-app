80129c84827f70061315130e5d9d4d6010b5febd
Aditya A <abujaditya82@gmail.com>
2025-04-11 23:43:36 +0530

    start

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signup=(req,res)=>{
+    res.send("signup router");
+};
+
+export const login=(req,res)=>{
+    res.send("signup router");
+};
+export const logout=(req,res)=>{
+    res.send("signup router");
+};
